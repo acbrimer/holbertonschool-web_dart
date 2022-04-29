01b3113a0ecacd094025d28eff2048ff967df302
Adam <arbimer@mac.com>
2022-04-29 10:16:09 -0500

    Fix for task 5

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -4,7 +4,7 @@ void main(List<String> args) {
    */
   String errorMessage =
       'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80';
-  int i = int.parse(args[0]);
-  assert(i >= 80, errorMessage);
+  int nb = int.parse(args[0]);
+  assert(nb >= 80, errorMessage);
   print('You Passed');
 }
